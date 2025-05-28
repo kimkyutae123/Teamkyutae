c8ad317fc63d1eebc2c8662a62b6211f5a5e540e
kimkyutae123 <136193399+kimkyutae123@users.noreply.github.com>
2025-05-28 09:48:56 +0900

    Update MemberID.sql

--- MemberID.sql
+++ MemberID.sql
@@ -1,9 +1,27 @@
 CREATE DATABASE Member;
 CREATE TABLE MemberID (
   user_id INT AUTO_INCREMENT PRIMARY KEY,
-  userage VARCHAR(20) NOT NULL,
-  user_email varchar(20) NOT NULL
+  name VARCHAR(20) NOT NULL
 );
+Create Table groupcreate (
+  Group_id INT AUTO_INCREMENT PRIMAARY KEY,
+  Group_name VARCHAR(20) NOT NULL,
+  foreign key (id) references MemberID(user_id)
+  );
+  Create table GroupAgree(
+    Agree_ID INT AUTO_INCREMENT PRIMARY KEY,
+    status BOOLEAN
+    );
+  Create table GroupChat(
+    Room_id INT NOT NULL PRIMARY KEY,
+    GroupRoom_user INT NOT NULL,
+    Message_text VARCHAR(100),
+    send_id INT NOT NULL,
+    foreign key (Group_id) references Group_id(groupcreate),
+    foreign key (id) references user_id(MemberID)
+    );
+
+
 
 USE Member;
 select *from MemberID;
