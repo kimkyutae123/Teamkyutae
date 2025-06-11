2600119a8a55872b232c043f253fcf900081933c
kyutae_kim <kendm1@naver.com>
2025-06-11 17:59:18 +0900

    databasefolder

--- AndroidProject/src/main/java/com/example/demo/repository/GroupChatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.GroupChat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface GroupChatRepository extends JpaRepository<GroupChat, Integer> {
-    List<GroupChat> findByGroup_GroupId(Integer groupId);
-}
--- AndroidProject/src/main/java/com/example/demo/repository/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface GroupRepository extends JpaRepository<Group, Integer> {
-    List<Group> findByMember_UserId(Integer userId);
-}
